f3ca234c9c559c5a832318d399e55ef8ac9428ad
rizqanhasanan <rizqanhalalandayyiban@gmail.com>
2022-11-19 05:13:44 -0800

    task

--- shell.h
+++ shell.h
@@ -1,18 +1,27 @@
 #ifndef _SHELL_H_
 #define _SHELL_H_
 
-#include <stdio.h>
+/*
+ * File: shell.h
+ * Abdurrazak Haruna 
+ * solomon Edziah
+ */
+
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <limits.h>
-#include <fcntl.h>
 #include <errno.h>
+#include <stdio.h>
 
-/* for read/write buffers */
+
+#define END_OF_FILE -2
+#define EXIT -3
+
+* for read/write buffers */
 #define READ_BUF_SIZE 1024
 #define WRITE_BUF_SIZE 1024
 #define BUF_FLUSH -1
